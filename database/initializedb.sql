2ca2ef1d6096ee0183d4118e378e4c9f15ad35bd
Joshua N. Ruiz <joshuanruiz@gmail.com>
2021-01-13 23:43:19 -0500

    intermediary work

--- database/initializedb.sql
+++ database/initializedb.sql
@@ -1,3 +1,5 @@
+DROP TABLE users,blackjack_game;
+
 CREATE TABLE users(
 	user_id INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(12),
@@ -6,20 +8,21 @@ CREATE TABLE users(
 
 CREATE TABLE blackjack_game(
 	blackjack_game_id INT NOT NULL AUTO_INCREMENT,
+    user_id INT NOT NULL,
     deck VARCHAR(500),
-    in_play VARCHAR(500),
     player_hand VARCHAR(500),
     dealer_hand VARCHAR(500),
-    PRIMARY KEY(blackjack_game_id)
+    PRIMARY KEY(blackjack_game_id),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
-CREATE TABLE user_blackjack_game(
-	user_id INT NOT NULL,
-    blackjack_game_id INT NOT NULL,
-	PRIMARY KEY(user_id, blackjack_game_id),
-    FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (blackjack_game_id) REFERENCES blackjack_game(blackjack_game_id)
-);
+-- CREATE TABLE user_blackjack_game(
+-- 	user_id INT NOT NULL,
+--     blackjack_game_id INT NOT NULL,
+-- 	PRIMARY KEY(user_id, blackjack_game_id),
+--     FOREIGN KEY (user_id) REFERENCES users(user_id),
+--     FOREIGN KEY (blackjack_game_id) REFERENCES blackjack_game(blackjack_game_id)
+-- );
 
 INSERT INTO users(username) VALUES ("tokyo");
 INSERT INTO users(username) VALUES ("ruiz");
--- server/server.js
+++ server/server.js
@@ -5,79 +5,79 @@ const app = express();
 const path = require('path');
 const bodyParser = require('body-parser');
 const mysql = require('mysql');
-const { connect } = require('http2');
+const Deck = require('./model/Deck');
+const e = require('express');
 
 app.use(express.static(path.join(__dirname, '../client/build')));
 app.use(bodyParser.json());
 
-const con = mysql.createConnection({
+const connection = mysql.createConnection({
     host: process.env.DATABASE_HOST,
     user: process.env.DATABASE_USER,
     password: process.env.DATABASE_PASS,
     database: process.env.DATABASE_TARGET
 });
 
-const draw = (userId, quantity) => {
-    // open connection to database
-    con.connect(err => {
-        if (err) throw err;
-        const sql = "SELECT deck FROM blackjack_game WHERE ";
-        con.query(sql, (err, result) => {
-            if (err) throw err;
-            console.log("Result: " + result[0].username + result[0].user_id);
-        })
-    })
-    // get the deck that the user is using
-    // take off the last [2-3] characters and make
-    // card object with that
-}
-
-const buildDeck = () => {
-    let deck = [];
-    for (let i = 1; i <= 13; i++) {
-        deck.push(i + 'c');
-        deck.push(i + 's');
-        deck.push(i + 'h');
-        deck.push(i + 'd');
-    }
-    shuffleArray(deck);
-    return deck.join('');
-}
-
-const shuffleArray = (array) => {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-}
-
 app.post('/api/new-game', (req, res) => {
     const userId = req.body.user_id;
-    const deck = buildDeck();
+    const deckCount = req.body.deck_count
+    const deck = new Deck(deckCount).toString();
 
     try {
-        con.connect(err => {
-            if (err) throw err;
-            const sql = "INSERT INTO blackjack_game (deck, in_play, player_hand, dealer_hand) VALUES ?";
-            const values = [[deck, "", "", ""]];
-            con.query(sql, [values], (err, result) => {
-                if (err) throw err;
-                const game_id = result.insertId;
-                const updateUserGame = `INSERT INTO user_blackjack_game (user_id, blackjack_game_id) VALUES (${userId},${game_id})`;
-                con.query(updateUserGame, (err, result) => {
-                    if (err) throw err;
-                    console.log("association made");
-                })
-                console.log("one entry inserted");
-            });
-        })
-    } catch (e) {
-        console.log(e);
-    } finally {
-        
-    }
-    res.send({message: "success"});
+        const createGameSql = `INSERT INTO blackjack_game (user_id, deck) VALUES (${userId},"${deck.toString()}")`;
+        connection.query(createGameSql, (error, result) => {
+            if (!error) {
+                console.log("new blackjack game created");
+            } else {
+                throw error;
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    } 
+
+    res.send();
 });
+
+app.post('/api/draw', (req, res) => {
+    const quantity = req.body.quantity;
+    const userId = req.body.user_id;
+    const hand = req.body.hand;
+
+});
+
+const draw = (userId) => {
+    const hand = 'player_hand';
+    let retrieveDeckSql = `SELECT deck, ${hand} FROM blackjack_game 
+        JOIN users ON blackjack_game.user_id = users.user_id 
+        WHERE users.user_id = ${userId}`;
+
+    try {
+        connection.query(retrieveDeckSql, (error, result) => {
+            if (!error) {
+                let [deck, card] = result[0].deck.split(/(\d+[a-z]$)/)
+                const currentHand = hand == "player_hand" ? 
+                // x_hand
+
+                // const newHand = result[0].player_hand ? result[0].player_hand + card : card;
+                console.log(newHand);
+
+                const updateBlackjackGameSql = `UPDATE blackjack_game SET deck = "${deck}", ${hand} = "${newHand}" WHERE user_id = ${userId}`;
+                connection.query(updateBlackjackGameSql, (error, result) => {
+                    if (!error) {
+                        console.log("success");
+                    } else {
+                        throw error;
+                    }
+                })
+            } else {
+                throw error;
+            }
+        })
+    } catch (error) {
+        console.log(error)
+    }
+}
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/build/index.html'));
@@ -85,5 +85,5 @@ app.get('*', (req, res) => {
 
 app.listen(8000, () => {
     console.log('listening on port 8000');
-    console.log(buildDeck());
+
 });
